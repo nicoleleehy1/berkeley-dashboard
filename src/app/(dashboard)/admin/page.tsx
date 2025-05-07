1f087207253e79492046c8c9455d22255eb05323
Nicole <nicole.lee@berkeley.edu>
2025-05-07 04:18:15 -0700

    calendar and progress tracker

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,21 +1,46 @@
+import EventCalendar from "@/components/EventCalendar"
 import UserCard from "@/components/UserCard"
+import InternshipChart from "@/components/InternshipChart"
 
 const AdminPage = () => {
   return (
     <div className='p-4 flex gap-4 flex-col md:flex-row'>
       {/* LEFT */}
-      <div className='w-full lg:w-2/3'>
+      <div className='w-full lg:w-2/3 flex flex-col gap-8'>
+      <span className="text-[20px]">📌 Pinned Classes</span>
       {/* USER CARDS */}
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="Math 54"/>
-          <UserCard type="teacher"/>
-          <UserCard type="parent"/>
-          <UserCard type="staff"/>
+          <UserCard type="CS 61B"/>
+          <UserCard type="EECS 16A"/>
+          <UserCard type="BIOE 25"/>
+        </div>
+
+        {/* MIDDLE CHARTS */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* PROGRESS TRACKER CHART */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <InternshipChart />
+          </div>
+
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            
+          </div>
+
+        </div>
+
+        {/* BOTTOM CHARTS */}
+        <div className="">
+
         </div>
       </div>
       
       {/* RIGHT */}
-      <div className='w-full lg:w-1/3'>r</div>
+      <div className='w-full lg:w-1/3 flex flex-col gap-8'>
+        <EventCalendar />
+    
+      </div>
     </div>
   )
 }
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -3,12 +3,12 @@ import Image from "next/image"
 const UserCard = ({type}:{type:string}) => {
   return (
     <div className='rounded-2xl odd:bg-dashboardPurple even:bg-dashboardYellow p-4 flex-1 min-w-[130px]'>
-        <div className="">
-            <span>2024/25</span>
+        <div className="flex justify-between items-center">
+            <span className="text-[10px] bg-white px-2 py-1 rounded-full text-green-600">2024/25</span>
             <Image src="/more.png" alt="" width={20} height={20} />
         </div>
-        <h1>1,234</h1>
-        <h1>{type}</h1>
+        <h1 className="text-2xl font-semibold my-4">{type}</h1>
+        <h2 className="capitalize text-sm font-medium text-gray-500">1234</h2>
     </div>
   )
 }
